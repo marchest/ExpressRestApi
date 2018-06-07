6e313faead2acb236cc57547596377794fa19fe9
Furkan Kaya <marchest21@gmail.com>
2018-06-07 22:10:50 +0300

    minor fix added

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ var express=require('express');
 var bodyParser=require('body-parser');
 var cookieParser=require('cookie-parser');
 var app=express();
-
+/*
+Furkan Kaya
+*/
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
