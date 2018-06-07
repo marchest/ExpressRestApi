19bfc9bcaaa134fe34ed84b02879f1d7cb5115b8
Furkan Kaya <marchest21@gmail.com>
2018-06-07 21:52:59 +0300

    express basic rest service

---
+++ index.js
@@ -0,0 +1,15 @@
+var express=require('express');
+var bodyParser=require('body-parser');
+var cookieParser=require('cookie-parser');
+var app=express();
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+var movies=require('./movies.js');
+
+app.use('/movies',movies);
+
+
+app.listen(3000);
